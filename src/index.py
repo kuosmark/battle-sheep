f574d0c8d0fe8bf2a90219f65967cfb1198a203e
Markus Kuosmanen <markus.kuosmanen@robu.fi>
2024-03-20 14:54:30 +0200

    Lisätään pelille geneerinen suoritussilmukka

--- src/index.py
+++ src/index.py
@@ -1,1 +1,9 @@
-print("Hello sheep!")
+running = True
+
+# Pelin suoritus
+while running:
+    print('Peli on käynnissä')
+
+    command = input("Syötä komento: ")
+    if command == 'lopeta':
+        running = False
